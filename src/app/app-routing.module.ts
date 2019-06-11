4f02d2eb1e5955f93c42efcfa803b292592df445
at-betruong <be.truong@asiantech.vn>
2019-06-11 13:49:35 +0700

    lazy load, fix page notfound

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FeaturesComponent } from './features/features.component';
 import { AuthComponent } from './auth/auth.component';
-import { AccountComponent } from './account/account.component';
+// import { AccountComponent } from './account/account.component';
 import { CanDeactivateGuard } from './auth/can-deactivate.guard';
 import { NotFoundComponent } from './features/not-found/not-found.component';
 
@@ -12,18 +12,21 @@ const routers: Routes = [
     component: FeaturesComponent
   },
   {
-    path: 'auth',
+    path: '',
     component: AuthComponent,
   },
   {
-    path: 'account',
-    component: AccountComponent,
+    // path: 'account',
+    // component: AccountComponent,
+
+    // lazy load
+    path: '',
+    loadChildren: './account/account.module#AccountModule'
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
   }
-  // ,
-  // {
-  //   path: '**',
-  //   component: NotFoundComponent,
-  // }
 ];
 @NgModule({
   imports: [
